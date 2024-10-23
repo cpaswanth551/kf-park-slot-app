c7c527bd4b2e695d7438cfe99a389d7166e1422f
cpaswanth551 <cpaswanth551@gmail.com>
2024-10-23 10:04:44 +0530

    runtime text added

--- app/main.py
+++ app/main.py
@@ -31,3 +31,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
