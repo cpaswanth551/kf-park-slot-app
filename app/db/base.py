dc312a069d1490a991fc06e1b9b7a5009c4aebcb
cpaswanth551 <cpaswanth551@gmail.com>
2024-10-23 18:40:45 +0530

    postgresql added

--- app/db/base.py
+++ app/db/base.py
@@ -4,11 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./parkapp.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://justparkdb_owner:xgvL79EwbByR@ep-tiny-hall-a1c6p0so.ap-southeast-1.aws.neon.tech/justparkdb?sslmode=require"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
